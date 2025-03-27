40ce177293c33e372b59dc5b359840159bfd5596
sagar9685 <sagargupta12396@gmail.com>
2025-03-27 18:00:45 +0530

    add fun

---
+++ src/store/shop/products-slice/index.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  productList: [],
+};
+
+export const fetchAllFilteredProducts = createAsyncThunk(
+  "/products/fetchAllProducts",
+  async () => {
+    const result = await axios.get(
+      "http://localhost:5000/api/shop/products/get"
+    );
+    console.log("Fetched Data:", result.data); // Check the exact structure
+    return result.data; // Ensure this matches expected Redux format
+  }
+);
+
+const shoppingProductsSlice = createSlice({
+  name: "shoppingProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllFilteredProducts.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
+        console.log(action.payload, "action.payload");
+        (state.isLoading = false), (state.productList = action.payload.data);
+      })
+      .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productList = [];
+      });
+  },
+});
+
+export default shoppingProductsSlice.reducer;
